3a1724ac7a6f5e9e66dd01d86c590e561f1a88da
Unis1809 <138216140+Unis1809@users.noreply.github.com>
2024-03-03 21:12:35 +0200

    last

--- 8-Puzzle Game.py
+++ 8-Puzzle Game.py
@@ -1,17 +1,19 @@
+import time
 import tkinter as tk
 from collections import deque
 from heapq import heapify, heappush, heappop
 import math
 
 class PuzzleState:
-    def __init__(self, state, parent=None, action=None, cost=0):
+    def __init__(self, state, parent=None, action=None, costType=0):
         self.state = state
         self.parent = parent
         self.action = action
-        if cost == 1:
-            self.cost = self.get_manhattan_distance(self.state)
-        elif cost == 2:
-            self.cost = self.get_euclidean_distance(self.state)
+        self.costType = costType
+        if costType == 1:
+            self.cost = self.get_manhattan_distance(self.state) + parent.cost if parent else self.get_manhattan_distance(self.state)
+        elif costType == 2:
+            self.cost = self.get_euclidean_distance(self.state) + parent.cost if parent else self.get_euclidean_distance(self.state)
 
     def __eq__(self, other):
         return self.state == other.state
@@ -46,11 +48,20 @@ class PuzzleState:
                 cost += abs(i - goal_row) + abs(j - goal_col)
         return cost
 
+    def decreaseKey(self,node):
+        if self.costType == 1:
+            self.cost = self.get_manhattan_distance(self.state) + node.cost
+        elif self.costType == 2:
+            self.cost = self.get_euclidean_distance(self.state) + node.cost
+
+        self.parent = node
+
+
 class Game:
-    def __init__(self, initial_state):
+    def __init__(self):
         self.root = tk.Tk()
         self.root.title("8-Puzzle Solver")
-        self.random_initial_state = initial_state
+        self.random_initial_state = None
         self.goal_state = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
         self.solution_path = None
 
@@ -62,47 +73,82 @@ class Game:
                                  borderwidth=1, font=("Helvetica", 16), bg="lightblue", fg="darkblue")
                 label.grid(row=i, column=j, padx=5, pady=5)
 
+        self.initial_state_entry = tk.Entry(self.root, font=("Helvetica", 12), justify="center")
+        self.initial_state_entry.grid(row=3, columnspan=3, pady=10)
+
+        submit_button = tk.Button(self.root, text="Submit Initial State", command=self.submit_initial_state,
+                                  bg="gray", fg="white", font=("Helvetica", 12), relief="raised")
+        submit_button.grid(row=4, columnspan=3, pady=10)
+
         self.arrow_label = tk.Label(self.root, text="", font=("Arial", 20), bg="lightgreen", fg="darkgreen")
-        self.arrow_label.grid(row=3, column=1, padx=5, pady=5, sticky='nsew')
+        self.arrow_label.grid(row=5, columnspan=3, pady=10, sticky='nsew')
 
         bfs_button = tk.Button(self.root, text="BFS", command=lambda: self.run_algorithm(1), bg="orange", fg="white",
                                font=("Helvetica", 14), relief="raised")
-        bfs_button.grid(row=4, column=0, padx=10, pady=10, sticky='nsew')
+        bfs_button.grid(row=6, column=0, padx=10, pady=10, sticky='nsew')
 
         dfs_button = tk.Button(self.root, text="DFS", command=lambda: self.run_algorithm(2), bg="purple", fg="white",
                                font=("Helvetica", 14), relief="raised")
-        dfs_button.grid(row=4, column=1, padx=10, pady=10, sticky='nsew')
+        dfs_button.grid(row=6, column=1, padx=10, pady=10, sticky='nsew')
 
         a_star_manhattan_button = tk.Button(self.root, text="A* Manhattan", command=lambda: self.run_algorithm(3),
                                             bg="green", fg="white", font=("Helvetica", 14), relief="raised")
-        a_star_manhattan_button.grid(row=4, column=2, padx=10, pady=10, sticky='nsew')
+        a_star_manhattan_button.grid(row=6, column=2, padx=10, pady=10, sticky='nsew')
 
         a_star_euclidean_button = tk.Button(self.root, text="A* Euclidean", command=lambda: self.run_algorithm(4),
                                             bg="blue", fg="white", font=("Helvetica", 14), relief="raised")
-        a_star_euclidean_button.grid(row=4, column=3, padx=10, pady=10, sticky='nsew')
+        a_star_euclidean_button.grid(row=6, column=3, padx=10, pady=10, sticky='nsew')
 
-        # Center the grid and buttons
-        for i in range(5):
+        for i in range(7):
             self.root.grid_rowconfigure(i, weight=1)
             self.root.grid_columnconfigure(i, weight=1)
 
         self.root.mainloop()
 
+    def submit_initial_state(self):
+        input_text = self.initial_state_entry.get()
+        try:
+            self.random_initial_state = [[int(char) for char in row] for row in input_text.split(',')]
+            self.update_gui_with_state(self.random_initial_state)
+        except ValueError:
+            print("Invalid input. Please enter a valid comma-separated 3x3 matrix.")
+
+    def update_gui_with_state(self, state):
+        for i in range(3):
+            for j in range(3):
+                self.cell_var[i][j].set(state[i][j])
+
     def run_algorithm(self, algorithm):
+        start = 0
+        end = 0
         if algorithm == 1:
+            start = time.time()
             self.solution_path = self.bfs(self.random_initial_state, self.goal_state)
+            end = time.time()
             self.root.title("8-Puzzle Solver BFS")
         elif algorithm == 2:
+            start = time.time()
             self.solution_path = self.dfs(self.random_initial_state, self.goal_state)
+            end = time.time()
             self.root.title("8-Puzzle Solver DFS")
         elif algorithm == 3:
+            start = time.time()
             self.solution_path = self.A_star(self.random_initial_state, self.goal_state, cost=1)
+            end = time.time()
             self.root.title("8-Puzzle Solver A* Manhattan")
         elif algorithm == 4:
+            start = time.time()
             self.solution_path = self.A_star(self.random_initial_state, self.goal_state, cost=2)
+            end = time.time()
             self.root.title("8-Puzzle Solver A* Euclidean")
 
-        self.update_gui_with_solution_path()
+        print("Time Taken by Algorithm = " + str(end - start))
+
+        if self.solution_path is not None:
+            self.update_gui_with_solution_path()
+        else:
+            # Display a message in the GUI indicating no solution
+            self.arrow_label.config(text="No Solution")
 
     def bfs(self, initial_state, goal_state):
         start_node = PuzzleState(initial_state)
@@ -115,9 +161,9 @@ class Game:
             current_node = queue.popleft()
 
             if current_node == goal_node:
-                print("Amount of nodes visited = " + str(len(visited)))
+                print("Amount of nodes visited by BFS = " + str(len(visited)))
                 path = self.get_path(current_node)
-                print("Amount of nodes in Path = " + str(len(path)))
+                print("Amount of nodes in Path by Bfs = " + str(len(path)))
                 return path
 
             if current_node not in visited:
@@ -132,19 +178,14 @@ class Game:
         return None
 
     def dfs(self, initial_state, goal_state):
-        #hna code aldfs
-        return None
+        start_node = PuzzleState(initial_state)
+        goal_node = PuzzleState(goal_state)
 
-    def A_star(self, initial_state, goal_state, cost):
-        start_node = PuzzleState(initial_state, cost=cost)
-        goal_node = PuzzleState(goal_state, cost=cost)
-
-        heap = [start_node]
-        heapify(heap)
+        stack = [start_node]
         visited = set()
 
-        while heap:
-            current_node = heappop(heap)
+        while stack:
+            current_node = stack.pop()
 
             if current_node == goal_node:
                 print("Amount of nodes visited = " + str(len(visited)))
@@ -157,9 +198,40 @@ class Game:
 
                 neighbors = self.get_neighbors(current_node.state)
                 for last_move, new_position, neighbor_state, move in neighbors:
-                    neighbor_node = PuzzleState(neighbor_state, current_node, move, cost=cost)
+                    neighbor_node = PuzzleState(neighbor_state, current_node, move)
                     if neighbor_node not in visited:
+                        stack.append(neighbor_node)
+
+        return None
+
+    def A_star(self, initial_state, goal_state, cost):
+        start_node = PuzzleState(initial_state, costType=cost)
+        goal_node = PuzzleState(goal_state, costType=cost)
+
+        heap = [start_node]
+        heapify(heap)
+        visited = set()
+
+        while heap:
+            current_node = heappop(heap)
+
+            if current_node == goal_node:
+                print("Amount of nodes visited by A* = " + str(len(visited)))
+                path = self.get_path(current_node)
+                print("Amount of nodes in Path by A* = " + str(len(path)))
+                print("Cost by A* = " + str(current_node.cost))
+                return path
+
+            if current_node not in visited:
+                visited.add(current_node)
+
+                neighbors = self.get_neighbors(current_node.state)
+                for last_move, new_position, neighbor_state, move in neighbors:
+                    neighbor_node = PuzzleState(neighbor_state, current_node, move, costType=cost)
+                    if neighbor_node not in visited and neighbor_node not in heap:
                         heappush(heap, neighbor_node)
+                    elif neighbor_node in heap:
+                        neighbor_node.decreaseKey(current_node)
 
         return None
 
@@ -216,5 +288,4 @@ class Game:
             self.root.after(1000, self.update_gui_with_solution_path)
 
 # Example usage with a random initial state
-random_initial_state = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
-game = Game(random_initial_state)
+game = Game()
